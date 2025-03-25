3efa4262b40c9120938b1322c62d9bc0a600627b
Pt Arman Badyal <arman53835@gmail.com>
2025-03-26 00:56:40 +0530

    added feedforward function
    ->added feedforward function and removed display structure function as it was not needed anymore 
    ->in order to effectively create feedforward i also did: 
         => added value variable in Neuron struct
         => (int) to (double) for weights and biases 
         => changed rand() to (rand()/(double)RAND_MAX) 
         =>

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 // Defining Structures
 
 // Structure to represent a weight node in the linked list
 struct WeightNode{
-    int weight;
+    double weight;
     struct WeightNode *next;
     struct WeightNode *prev;
 };
@@ -16,7 +17,8 @@ typedef struct WeightNode Weight;
 // Structure to represent a neuron in the network
 struct Neuron{
     Weight* weightNode;
-    int bias; // Bias value for the neuron
+    double bias; // Bias value for the neuron
+    double value; // output of each neuron
     struct Neuron *next;
     struct Neuron *prev;
 };
@@ -35,12 +37,15 @@ typedef struct Layer Layer;
 
 // Function declarations
 void initializeNetwork(int [], int);
-int displayNetwork();
+double sigmoid(double);
+void feedForward(int []);
+void displayFinalOutput();
+
 
 
 // Global variables for network configuration
-int n = 3; // Number of layers
-int network_structure[] = {1,5,1}; // Number of neurons per layer
+int n = 4; // Number of layers
+int network_structure[] = {3, 5, 5, 2}; // Number of neurons per layer
 Layer *Network = NULL; // Head of the network linked list
 
 
@@ -50,9 +55,14 @@ int main(){
 
     // Initializing the network
     initializeNetwork(network_structure, n);
-    
-    // Display the network structure
-    displayNetwork();
+    // example input
+    int input[] = {3, 7, 12};
+
+    //perform feedforward
+    feedForward(input);
+    //to see final outputs
+    displayFinalOutput();
+
 
     return 0;
 }
@@ -61,53 +71,56 @@ int main(){
 // Function to initialize the neural network
 void initializeNetwork(int structure[], int n1){
     int num = 0;
-    
+
     printf("\nInitializing Neural Network\n");
 
     Layer* head = NULL; // Head of the layer linked list
     Layer* tail = NULL; // Tail pointer to keep track of last layer
-    
+
 
     // Adding layers to the network
     for(int i = 0; i < n1; i++){
         Layer* l = (Layer*) malloc(sizeof(Layer)); // Allocate memory for new layer
         num = structure[i]; // Number of neurons in current layer
-        
+
         Neuron *n = NULL; // Head of neuron linked list
         Neuron *n_tail = NULL; // Tail pointer for neurons
-        
+
 
         // Adding neurons to the layer
         for(int j = 0; j < num; j++){
 
             Neuron* tempNeuron = (Neuron*) malloc(sizeof(Neuron)); // Allocate memory for neuron
-            tempNeuron->bias = rand(); // Assign a random bias
-            
+            tempNeuron->bias = (rand() / (double)RAND_MAX) - 0.5; // Assign a random bias
+
             Weight *w = NULL; // Head of weight linked list
             Weight *w_tail = NULL; // Tail pointer for weights
-            
+
 
             // Assigning random weights (except for the first layer, which doesn't need weights)
-            for(int k = 0; k < network_structure[i-1]; k++){
+            if (i>0) {
+                for(int k = 0; k < network_structure[i-1]; k++){
 
-                Weight *tw = (Weight*)malloc(sizeof(Weight)); // Allocate memory for weight node
-                tw->weight = rand();  // Assign a random weight
-                
-                // Insert at the end of the weight linked list
-                if(w == NULL){
-                    w = tw;
-                    w_tail = tw;
-                    tw->prev = NULL;
-                    tw->next = NULL;
-                } else {
-                    tw->prev = w_tail;
-                    w_tail->next = tw;
-                    w_tail = tw;
-                    w_tail->next = NULL;
+                    Weight *tw = (Weight*)malloc(sizeof(Weight)); // Allocate memory for weight node
+                      // Assign a random weight
+                    tw->weight = (rand() / (double)RAND_MAX) - 0.5;  // ✅ Correct scaling
+
+                    // Insert at the end of the weight linked list
+                    if(w == NULL){
+                        w = tw;
+                        w_tail = tw;
+                        tw->prev = NULL;
+                        tw->next = NULL;
+                    } else {
+                        tw->prev = w_tail;
+                        w_tail->next = tw;
+                        w_tail = tw;
+                        w_tail->next = NULL;
+                    }
+
                 }
-
             }
-            
+
             tempNeuron->weightNode = w; // Attach the weight linked list to neuron
 
             // Insert neuron at the end of the neuron linked list
@@ -122,7 +135,7 @@ void initializeNetwork(int structure[], int n1){
                 n_tail = tempNeuron;
                 n_tail->next = NULL;
             }
-            
+
         }
 
         l->neuron = n; // Attach neuron linked list to layer
@@ -144,48 +157,50 @@ void initializeNetwork(int structure[], int n1){
     Network = head; // Set the global network head pointer
     printf("Done\n\n");
 }
-
-// Function to display the network structure
-int displayNetwork(){
-    if(Network == NULL){
-        return printf("Network is Empty");
+// Activation function (Sigmoid)
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
+// Feedforward function
+void feedForward(int input[]){
+    Layer* currentLayer = Network;
+    Neuron* n = currentLayer->neuron;
+    // giving input values
+    for(int i = 0; i < network_structure[0]; i++){
+      n->value = (double)input[i];
+      n = n->next;
     }
-    
-    printf("Below is the structure of the network with random initialization.\n\n");
-    printf("Format:\n-> Layer Address\n    -> Neurons' Bias\n        -> Weights\n\n");
-    printf("\nStructure:\n\n");
-    
-    int i = 0;
-    Layer* head = Network;
-    
-    // Traverse each layer
-    while (head != NULL){
-        printf("-> Layer %p\n", head);
-        Neuron* tn = head->neuron;
-        
-        // Traverse each neuron in the layer
-        for(int j = 0; j < network_structure[i]; j++){
-            printf("    -> Bias: %d", tn->bias);
-
-            // Print weights if not the first layer
-            if(i != 0){
-                Weight *tw = tn->weightNode;
-                printf("\n        Weights: ");
-                while(tw != NULL){
-                    printf("-> %d", tw->weight);
-                    tw = tw->next;
-                }
-                printf("\n");
-            } else {
-                printf("\n        No Weights Required\n");
-            }
-            
-            tn = tn->next;
+    currentLayer = currentLayer->next;
+    int h = 1;
+    while(currentLayer != NULL){
+      Neuron* currentNeuron = currentLayer->neuron;
+      for(int i = 0; i < network_structure[h]; i++){
+        double value = 0.0;
+        Neuron* previousLayerNeuron = currentLayer->prev->neuron;
+        Weight* w = currentNeuron->weightNode;
+        for(int j = 0; j < network_structure[h-1]; j++){
+          value += (w->weight)*(previousLayerNeuron->value);
+          previousLayerNeuron = previousLayerNeuron->next;
+          w = w->next;
         }
-        printf("\n");
-        i++;
-        head = head->next;
+        currentNeuron->value = sigmoid(value+(currentNeuron->bias));
+        currentNeuron = currentNeuron->next;
+      }
+      currentLayer = currentLayer->next;
+      h++;
     }
-    
-    return 0;
+}
+// i want to see the final outputs in the last layer
+void displayFinalOutput(){
+    Layer* lastLayer = Network;
+    while (lastLayer->next != NULL) {
+        lastLayer = lastLayer->next;
+    }
+    printf("Final Output:\n");
+    Neuron* outputNeuron = lastLayer->neuron;
+    while(outputNeuron != NULL){
+      printf("%lf\n", outputNeuron->value);
+      outputNeuron = outputNeuron->next;
+    }
+
 }
